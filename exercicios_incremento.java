17a285e4d474a7b8c5f4d30dee08fc4f73135b94
gabriellimacostodio <64055343+gabriellimacostodio@users.noreply.github.com>
2020-04-21 05:19:03 -0300

    Add files via upload

---
+++ exercicios_incremento.java
@@ -0,0 +1,26 @@
+
+public class exercicios_incremento {
+
+	public static void main(String[] args) {
+		
+		int a = 3, b = 2, c = 1, x = 0;
+				
+		System.out.println (x=--b - b-1);
+		System.out.println(c=a/b);
+		System.out.println(a += x + c * b++);
+		System.out.println(c = ++x + x *3 + x);
+		System.out.println(c = --x * 4 /b);
+		System.out.println(a /= --b);
+		System.out.println(x *= c++);
+		System.out.println(a = (--b *c) + (c++ - b));
+		System.out.println(++x);
+		
+		
+		System.out.println(a);
+	    System.out.println(b);
+		System.out.println(c);
+		System.out.println(x);
+		
+	}
+
+}
